2b38970a336f533cc8ac9149fccaa2af21fc4d25
Savant Krishna <savant.2020@gmail.com>
2013-08-23 22:22:09 +0530

    core part implemented, some tests and array full handling left

--- assgn1/1stacks.cpp
+++ assgn1/1stacks.cpp
@@ -1,23 +1,54 @@
 #include<iostream>
-#define MAX_SIZE 10
+#define DEFAULT_SIZE 128
 
 template <class T>
 class TwoStacks{
   public:
     TwoStacks();
+    TwoStacks(int);
     int getArraySize();
-    T pop1();
-    T pop2();
-    T push1();
-    T push2();
+    T pop(int);
+    void push(int, T);
   private:
-    T _array[MAX_SIZE];
+    T* _array;
     int _size;
+    int _pos[2];
+    void initializeArray();
 };
 
 template <class T>
 TwoStacks<T>::TwoStacks(){
-    _size = MAX_SIZE;
+    _size = DEFAULT_SIZE;
+    initializeArray();
+}
+
+template <class T>
+TwoStacks<T>::TwoStacks(int size){
+    _size = size;
+    initializeArray();
+}
+
+template <class T>
+void TwoStacks<T>::initializeArray(){
+    _array = new T[_size];
+    _pos[0] = 0;
+    _pos[1] = _size;
+}
+
+/* pop() implementation: takes 0 or 1 as integer input and pops the element of
+ * the first stack or the second
+ */
+template <class T>
+T TwoStacks<T>::pop(int sn){
+    T res = _array[_pos[sn]];
+    _pos[sn] = (sn==0)?_pos[sn]-1:_pos[sn]+1;
+    return res;
+}
+
+template <class T>
+void TwoStacks<T>::push(int sn, T data){
+    _pos[sn] = (sn==0)?_pos[sn]+1:_pos[sn]-1;
+    _array[_pos[sn]] = data;
 }
 
 template <class T>
@@ -25,7 +56,7 @@ int TwoStacks<T>::getArraySize(){
     return _size;
 }
 
-// Testing the TwoStacks
+// Testing the TwoStacks class
 int main(){
     TwoStacks<int> s;
     std::cout << s.getArraySize() << "\n";
